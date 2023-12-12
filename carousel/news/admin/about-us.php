2a286af8b1824b8001b3b823976cace7e81100d0
Ramsley Lichuma <113148758+RamsleyLichuma@users.noreply.github.com>
2023-12-12 23:28:13 +0300

    ...

---
+++ carousel/news/admin/about-us.php
@@ -0,0 +1,51 @@
+<?php
+include('includes/config.php');
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Mighty Eagles Soccer Academy  | About us</title>
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/modern-business.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/icons.css">
+  </head>
+  <body>
+    <div class="card my-4 border-0" style="text-align:center; height:100%; margin-left:0; margin-right:0; width:100%;" >
+      <?php include('includes/header.php');?>
+      </div>
+    <div class="container">
+<?php 
+$pagetype='aboutus';
+$query=mysqli_query($con,"select PageTitle,Description from tblpages where PageName='$pagetype'");
+while($row=mysqli_fetch_array($query))
+{
+
+?>
+      <h1 class="mt-5 mb-3 text-center"><?php echo htmlentities($row['PageTitle'])?> 
+      </h1>
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item">
+          <a href="index.php">Home</a>
+        </li>
+        <li class="breadcrumb-item active">About</li>
+      </ol>
+      <div class="row" >
+        <div class="col-lg-12">
+          <p style="align-content:center; text-align:center;"><?php echo $row['Description'];?></p>
+        </div>
+      </div>
+<?php } ?>   
+    </div>
+ <?php include('includes/footer.php');?>
+ <footer>
+            <p style="text-align:center;">&copy; Copyright <a href="https://www.linkedin.com/in/ramsley-lichuma-2b9186260/" style="color:blue; ">Xxxage Technologies Incorporated </a> @ 2023 </p>
+        </footer>
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
