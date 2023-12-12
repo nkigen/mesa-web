e48fceeae0827291062377b16abd677bb9d669b6
Nelson Kigen <nkigen@fameve.com>
2023-12-12 15:05:39 -0500

    update

--- carousel/news/includes/config.php
+++ carousel/news/includes/config.php
@@ -2,7 +2,7 @@
 define('DB_SERVER','terraform-20231002010411504000000008.ctjten6aavan.us-east-2.rds.amazonaws.com');
 define('DB_USER','mesa_user');
 define('DB_PASS' ,'ztX2JG3dL150');
-define('DB_NAME','newsportal');
+define('DB_NAME','mesa');
 
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
