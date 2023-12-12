36db9d544940546d2e07b7a4fda78d4d96bdebdd
Ramsley Lichuma <113148758+RamsleyLichuma@users.noreply.github.com>
2023-12-12 20:37:31 +0300

    update

---
+++ carousel/news/includes/banner.php
@@ -0,0 +1,108 @@
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Lichuma Ramsley, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.88.1">
+    <title> Mighty Eagles Soccer Academy </title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/headers/">
+
+    
+
+    <!-- Bootstrap core CSS -->
+<link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="headers.css" rel="stylesheet">
+
+    <title>v</title>
+        <link rel="icon" type="image/x-icon" href="assets/images/mighty.png">
+
+      <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+      <link href="assets/css/core.css" rel="stylesheet" type="text/css" />
+      <link href="assets/css/components.css" rel="stylesheet" type="text/css" />
+      <link href="assets/css/icons.css" rel="stylesheet" type="text/css" />
+      <link href="assets/css/pages.css" rel="stylesheet" type="text/css" />
+      <link href="assets/css/menu.css" rel="stylesheet" type="text/css" />
+      <link href="assets/css/responsive.css" rel="stylesheet" type="text/css" />
+      <link rel="stylesheet" href="../plugins/switchery/switchery.min.css">
+      
+        <!-- Summernote css -->
+        <link href="../plugins/summernote/summernote.css" rel="stylesheet" />
+      <!-- Select2 -->
+ 
+        <link href="../plugins/select2/css/select2.min.css" rel="stylesheet" type="text/css" />
+
+        <!-- Jquery filer css -->
+        <link href="../plugins/jquery.filer/css/jquery.filer.css" rel="stylesheet" />
+        <link href="../plugins/jquery.filer/css/themes/jquery.filer-dragdropbox-theme.css" rel="stylesheet" />
+      <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
+      <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.2.3/css/buttons.dataTables.min.css">
+      <script>
+         function checkAvailability() {
+         $("#loaderIcon").show();
+         jQuery.ajax({
+         url: "check_availability.php",
+         data:'username='+$("#sadminusername").val(),
+         type: "POST",
+         success:function(data){
+         $("#user-availability-status").html(data);
+         $("#loaderIcon").hide();
+         },
+         error:function (){}
+         });
+         }
+      </script>
+  </head>
+  <body>
+    
+  <header class="p-3 bg-dark text-white">
+    <div class="container">
+      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+          <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"/></svg>
+        </a>
+
+        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+          <li><a href="#" class="nav-link px-2 text-secondary">Home</a></li>
+          <li><a href="#" class="nav-link px-2 text-white">Features</a></li>
+          <li><a href="#" class="nav-link px-2 text-white">Pricing</a></li>
+          <li><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
+          <li><a href="#" class="nav-link px-2 text-white">About</a></li>
+        </ul>
+
+        <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+          <input type="search" class="form-control form-control-dark" placeholder="Search..." aria-label="Search">
+        </form>
+
+        <div class="text-end">
+          <button type="button" class="btn btn-outline-light me-2">Login</button>
+          <button type="button" class="btn btn-warning">Sign-up</button>
+        </div>
+      </div>
+    </div>
+  </header>
+
+    </body>
---
+++ carousel/news/includes/header.php
@@ -0,0 +1,43 @@
+<nav class="navbar fixed-top navbar-expand-lg navbar-light bg-white fixed-top">
+   <div class="container">
+      <a class="navbar-brand" href="index.php"><img src="images/mighty.png" height="65" style="align-self:left;"></a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon" ></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-center" id="navbarResponsive">
+         <ul class="navbar-nav">
+            <li class="nav-item">
+               <a class="nav-link" href="index.php"><i class="fa fa-home"></i> Home</a>
+            </li>
+            <li class="nav-item">
+               <a class="nav-link" href="about-us.php"><i class="fa fa-info-circle"></i> About</a>
+            </li>
+            <li class="nav-item">
+               <a class="nav-link" href="index.php"><i class="fa-newspaper-o fa"></i> News</a>
+            </li>
+            <li class="nav-item">
+               <a class="nav-link" href="contact-us.php"><i class="fa fa-phone"></i> Contact us</a>
+            </li>
+        
+            <li class="nav-item dropdown">
+               
+               <button type="button" class="btn  dropdown-toggle" data-toggle="dropdown">More</button>
+               <ul class="dropdown-menu" aria-labelledby="dropdown07XL">
+               
+                              <?php $query=mysqli_query($con,"select id,CategoryName from tblcategory");
+                                 while($row=mysqli_fetch_array($query))
+                                 {
+                                 ?>
+                              <li class=" mb-2" style="text-align:center;">
+                                 <a href="category.php?catid=<?php echo htmlentities($row['id'])?>" class="text-secondary"><?php echo htmlentities($row['CategoryName']);?></a>
+                              </li>
+                              <?php } ?>
+               </ul>
+            </li>
+        </ul>
+      </div>
+      <div>
+         <div id="google_translate_element"></div>
+      </div>
+   </div>
+</nav>
