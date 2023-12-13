51125359b93c951ded47137b30fe84f5e3f309f9
Ramsley Lichuma <113148758+RamsleyLichuma@users.noreply.github.com>
2023-12-13 08:08:24 +0300

    Update config.php

--- carousel/news/admin/includes/config.php
+++ carousel/news/admin/includes/config.php
@@ -1,8 +1,9 @@
 <?php
-define('DB_SERVER','localhost');
-define('DB_USER','root');
-define('DB_PASS' ,'');
-define('DB_NAME','newsportal');
+define('DB_SERVER','terraform-20231002010411504000000008.ctjten6aavan.us-east-2.rds.amazonaws.com');
+define('DB_USER','mesa_user');
+define('DB_PASS' ,'ztX2JG3dL150');
+define('DB_NAME','mesa');
+
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
 if (mysqli_connect_errno())
@@ -10,4 +11,3 @@ if (mysqli_connect_errno())
  echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 ?>
- 
